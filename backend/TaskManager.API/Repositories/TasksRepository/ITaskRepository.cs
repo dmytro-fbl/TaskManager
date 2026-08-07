b2b5031233b6c116390697c753cd3a33521a58cd
TymurKashchuk <t1mu4337@gmail.com>
2026-08-07 17:05:06 +0300

    feat(tasks): define task repository contract
    feat(tasks): define task repository contract

---
+++ backend/TaskManager.API/Repositories/TasksRepository/ITaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using TaskManager.API.Models.TasksTables;
+
+namespace TaskManager.API.Repositories.TasksRepository
+{
+    public interface ITaskRepository
+    {
+        Task<Guid> CreateTaskAsync(TaskItem task);
+
+        Task<TaskItem?> GetTaskByIdAsync(Guid taskId);
+
+        Task<IEnumerable<TaskItem>> GetProjectTasksAsync(Guid projectId);
+
+        Task<IEnumerable<TaskAssignment>> GetTaskAssignmentsAsync(Guid taskId);
+
+        Task<bool> AddTaskAssignmentAsync(TaskAssignment assignment);
+
+        Task<bool> RemoveTaskAssignmentAsync(Guid taskId, Guid userId);
+    }
+}
